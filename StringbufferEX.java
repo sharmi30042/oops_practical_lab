86fcd91cbf996dbea56511460ed590aeed5f1ac0
Pathi Sharmila <2420030042@klh.edu.in>
2025-08-02 11:35:54 +0530

    Add files via upload

---
+++ StringbufferEX.java
@@ -0,0 +1,29 @@
+package CSE;
+
+public class StringbufferEX {
+		public static void main(String[] args) {
+			    StringBuffer sb = new StringBuffer("Hello");
+			    sb.append(" World");
+		        System.out.println("After append: " + sb);
+		        sb.insert(5, ", Pretty");
+		        System.out.println("After insert: " + sb); 
+		        sb.replace(6, 13, " Green");
+		        System.out.println("After replace: " + sb);
+		        sb.delete(5, 15);
+		        System.out.println("After delete: " + sb);
+		        sb.reverse();
+		        System.out.println("After reverse: " + sb); 
+		        char ch = sb.charAt(4);
+		        System.out.println("Character at index 4: " + ch);
+		        sb.setCharAt(4, 'X');
+		        System.out.println("After setCharAt: " + sb);
+		        int len = sb.length();
+		        System.out.println("Length: " + len); 
+		        int cap = sb.capacity();
+		        System.out.println("Capacity: " + cap); 
+		        String str = sb.toString();
+		        System.out.println("Converted to String: " + str);
+		    }
+		}
+
+
